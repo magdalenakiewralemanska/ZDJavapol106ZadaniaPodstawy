45e802375f070446ea0ea25bb3e537bab3af852c
Magda K-L <magdalenazur86@gmail.com>
2022-02-20 13:17:45 +0100

    Task8

---
+++ src/pl/sdacademy/java/basic/exercises/day2/task8/Task8.java
@@ -0,0 +1,46 @@
+package pl.sdacademy.java.basic.exercises.day2.task8;
+
+public class Task8 {
+    private static final int DIFF_UPPER_AND_LOWER_IN_ASCII = 32;
+    private static final int VALUE_LOWER_A_IN_ASCII = 97;
+    private static final int VALUE_LOWER_Z_IN_ASCII = 122;
+    private static final int VALUE_UPPER_A_IN_ASCII = 65;
+    private static final int VALUE_UPPER_Z_IN_ASCII = 90;
+
+    public static void main(String[] args) {
+        String input = "AlaKot";
+        System.out.println("Input: " + input);
+        System.out.println("Result: " + replaceChars(input));
+        System.out.println("Result: " + changeSize(input));
+    }
+    private static String replaceChars (String input) {
+//        String result = "";
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < input.length(); i++) {
+            char currentChar = input.charAt(i);
+            if (currentChar >= VALUE_UPPER_A_IN_ASCII && currentChar <= VALUE_UPPER_Z_IN_ASCII) {
+//                result += (char)(currentChar + 32);
+                result.append((char)(currentChar + DIFF_UPPER_AND_LOWER_IN_ASCII));
+            } else if (currentChar >= VALUE_LOWER_A_IN_ASCII && currentChar <= VALUE_LOWER_Z_IN_ASCII) {
+//                result += (char) (currentChar - 32);
+                result.append((char)(currentChar - DIFF_UPPER_AND_LOWER_IN_ASCII));
+            } else {
+//                result += (char) currentChar;
+                result.append(currentChar);
+            }
+        }
+        return result.toString();
+    }
+
+    private static String changeSize(String text) {
+        String newText = "";
+        for (int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) == text.toUpperCase().charAt(i)){
+                newText += text.toLowerCase().charAt(i);
+            } else {
+                newText += text.toUpperCase().charAt(i);
+            }
+        }
+        return newText;
+    }
+}
